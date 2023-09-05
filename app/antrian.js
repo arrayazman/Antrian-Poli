665d04e2b55f00b0fe6daea8009596b7302a9126
Array <arrayazman@gmail.com>
2023-09-05 20:10:44 +0700

    Update Menampilkan Dokter Sesuai Jadwal Poli
    Update Menampilkan Dokter Sesuai Jadwal Poli

--- app/antrian.js
+++ app/antrian.js
@@ -1,65 +1,77 @@
 
-$(document).ready(function() { 
-    $.ajax({
-        url: 'antrian.php?p=pengaturan',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            var email= $('#email');
-            email.html(data.email);
-
-            var namars= $('#namars');
-            namars.html(data.nama_instansi);
-
-            var text= $('#text');
-            text.html(data.text);
-        }
-        
-    });
-});
-
 //=========================================================================
-    // Fungsi pemanggil
-    function Suara() {
-        $.ajax({
-          url: "antrian.php?p=panggil",
-          type: "GET",
-          dataType: "json",
+    // Menampilkan data rumah sakit
+    $(document).ready(function() { 
+      $.ajax({
+          url: 'antrian.php?p=pengaturan',
+          type: 'GET',
+          dataType: 'json',
           success: function(data) {
-            var nomorAntrian = $("#suara");
-            nomorAntrian.empty();
-      
-            $.each(data, function(index, item) {
-              // Suara notifikasi pemanggilan antrian
-              var audio = document.getElementById("myAudio");
-              audio.onended = function() {
-                // Callback yang akan dijalankan setelah audio selesai
-                responsiveVoice.speak(
-                  "Atas nama " + item.nm_pasien.toLowerCase() +
-                  ", Silahkan menuju " + item.nm_poli.toLowerCase(),
-                  "Indonesian Female", {
-                    pitch: 1,
-                    rate: 0.9,
-                    volume: 1
-                  }
-                );
-              };
-              // Memainkan suara notifikasi
-              audio.play();
-            });
-          },
+              var email= $('#email');
+              email.html(data.email);
+  
+              var namars= $('#namars');
+              namars.html(data.nama_instansi);
+  
+              var text= $('#text');
+              text.html(data.text);
+          }
+          
+      });
+  });
+  
+  //=========================================================================
+      // Fungsi pemanggil
+      function Suara() {
+          $.ajax({
+            url: "antrian.php?p=panggil",
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+              var nomorAntrian = $("#suara");
+              nomorAntrian.empty();
+        
+              $.each(data, function(index, item) {
+           
+  //========================================================================
+      //==display poli== 
+      function datapoli() {
+        $.ajax({
+            url: "antrian.php?p=poli",
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                var swiperWrapper = $("#datapoli");
+                swiperWrapper.empty();
+    
+                // Loop melalui data dan tambahkan slide
+                $.each(data, function(index, item) {
+                    var varpoli = $("<div class='col-lg-3 text-center '>"+
+                                    "<div class='card pt-2 border border-success'>"+
+                                    "<h5>" + item.nm_poli + "</h5><b>" + item.nm_dokter + "</b><br><p class='pasien'>");
+                                   
+                                   $.each(item.data_pasien, function(index, pasien) {
+                        var antrian_pasien = $("<h2>" + pasien.kd_poli + pasien.no_reg + "</h2>"+
+                        "<h5>" + pasien.nm_pasien + "</h5>");
+                        varpoli.find('.pasien').append(antrian_pasien);
+                    });  
+                    varpoli.append("</p></div></div>");  
+                    swiperWrapper.append(varpoli);
+                });
+            }, 
         });
-      }
+    }
 
-    //refresh otomatis setiap 8detik
-    setInterval(Suara, 8000); 
+    // Memanggil fungsi displaypoli() saat halaman pertama kali dimuat
     $(document).ready(function() {
-        Suara();
+        datapoli();
     });
-	
- //=======================================================================
-    // Fungsi display nomor
-    function DisplayNomor() {
+  
+  //=======================================================================
+     
+  //=======================================================================
+      // Fungsi display nomor
+      function DisplayNomor() {
         $.ajax({
             url: "antrian.php?p=nomor",
             type: "GET",
@@ -79,79 +91,75 @@ $(document).ready(function() {
             }, 
         });
     }
-    // Memanggil fungsi DisplayNomor() secara berkala (misalnya, setiap 2 detik)
-    setInterval(DisplayNomor, 2000);
+    
     // Memanggil DisplayNomor() saat halaman pertama kali dimuat
     $(document).ready(function() {
         DisplayNomor();
     });
-
-//==============================================================================
-    //==display poli==
-    function datapoli() {
-    $.ajax({
-        url: "antrian.php?p=poli",
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            var swiperWrapper = $("#datapoli");
-            swiperWrapper.empty();
-
-            // Loop melalui data dan tambahkan slide
-            $.each(data, function(index, item) {
-                var varpoli = $("<div class='col-lg-3 text-center '>"+
-                                "<div class='card pt-2 border border-success'>"+
-                                "<h5>" + item.nm_poli + "</h5><b>" + item.nm_dokter + "</b><br><p class='pasien'>");
-                               
-                               $.each(item.data_pasien, function(index, pasien) {
-                    var antrian_pasien = $("<h2>" + pasien.kd_poli + pasien.no_reg + "</h2>"+
-                    "<h5>" + pasien.nm_pasien + "</h5>");
-                    varpoli.find('.pasien').append(antrian_pasien);
-                });  
-                varpoli.append("</p></div></div>");  
-                swiperWrapper.append(varpoli);
-            });
-        }, 
-    });
-}
-
-// Menjalankan displaypoli() setiap 5000 milidetik (5 detik)
-setInterval(datapoli, 5000);
-// Memanggil fungsi displaypoli() saat halaman pertama kali dimuat
-$(document).ready(function() {
-    datapoli();
-});
-
-
-//==========membuat jam=============
- function updateClock() {
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-
-  // Format waktu dengan tambahkan "0" di depan angka jika kurang dari 10
-  hours = (hours < 10 ? "0" : "") + hours;
-  minutes = (minutes < 10 ? "0" : "") + minutes;
-  seconds = (seconds < 10 ? "0" : "") + seconds;
-
-  var timeString = ""+ hours + ":" + minutes + ":" + seconds;
-
-  // Update elemen HTML dengan waktu yang telah diformat
-  document.getElementById("clock").innerHTML = timeString;
-}
-
-// Panggil fungsi updateClock setiap detik
-setInterval(updateClock, 1000);
- 
- 
-//==================pengaturan video ===============
-  // Mengambil elemen video
-  var video = document.getElementById("myVideo");
-  // Mengatur volume ke 20%
-  video.volume = 0.2;
-  video.muted = false;
-//==================================================
-
-
- 
+  //========================================================================
+  
+  
+                // Suara notifikasi pemanggilan antrian
+                var audio = document.getElementById("myAudio");
+                audio.onended = function() {
+                  // Callback yang akan dijalankan setelah audio selesai
+                  responsiveVoice.speak(
+                    "Atas nama " + item.nm_pasien.toLowerCase() +
+                    ", Silahkan menuju " + item.nm_poli.toLowerCase(),
+                    "Indonesian Female", {
+                      pitch: 1,
+                      rate: 0.9,
+                      volume: 1
+                    }
+                  );
+                };
+                // Memainkan suara notifikasi
+                audio.play();
+  
+              });
+            },
+          });
+        }
+  
+  
+  
+      //refresh otomatis setiap 5 detik
+      setInterval(Suara, 5000); 
+      $(document).ready(function() {
+          Suara();
+      });
+    
+   //=======================================================================
+      
+  //==========membuat jam=============
+   function updateClock() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+  
+    // Format waktu dengan tambahkan "0" di depan angka jika kurang dari 10
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+  
+    var timeString = ""+ hours + ":" + minutes + ":" + seconds;
+  
+    // Update elemen HTML dengan waktu yang telah diformat
+    document.getElementById("clock").innerHTML = timeString;
+  }
+  
+  // Panggil fungsi updateClock setiap detik
+  setInterval(updateClock, 1000);
+   
+   
+  //==================pengaturan video ===============
+    // Mengambil elemen video
+    var video = document.getElementById("myVideo");
+    // Mengatur volume ke 40%
+    video.volume = 0.2;
+    video.muted = false;
+  //==================================================
+  
+  
+   
